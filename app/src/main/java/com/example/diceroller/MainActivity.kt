dc9320885ac0893ac682e5ec18decb7db53130a6
KoningHoningh <eelcohoningh@hotmail.com>
2022-06-22 19:04:57 +0200

    Added images to show the dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 
@@ -13,24 +15,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         /*
         This sets the button as a button and lets it be used.
          */
+
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener { rollDice() } // Generates the acitivity on the roll.
     }
+
     /*
         This creates the dice, rolls it and places the results into a textView
      */
     private fun rollDice() {
-        /*
 
-         */
-        var dice = Dice(6) //Creates a dice.
-        var diceRoll = dice.roll() //Rolls a dice.
-        var resultTextView: TextView = findViewById(R.id.textView) //Sets up a TextView for the result.
-        resultTextView.text = diceRoll.toString() //Appends the results to the textView.
-
+        val dice = Dice(6) //Creates a dice D6.
+        val diceRoll = dice.roll() //Rolls a dice.
+        val diceImage: ImageView = findViewById(R.id.imageView) //Sets up a Image for the result.
+        // Determines what to show when what outcome the dice has.
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
